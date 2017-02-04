d80b4c42078ae2125ffb3d755d690e4562b9c10b
miki208 <miloss208@gmail.com>
2017-02-04 02:03:48 +0100

    Add schedule for parsing and loading data

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'App\Console\Commands\LoadTvPrograms',
+        'App\Console\Commands\LoadSchedules'
     ];
 
     /**
@@ -24,6 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-
+        $schedule->command('load_schedules')->daily()->timezone('Europe/Belgrade');
     }
 }
